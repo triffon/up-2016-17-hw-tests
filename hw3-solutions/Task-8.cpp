34c4b72849806984818de06af1c67eba7ed21669
Petar Armianov <armianov@gmail.com>
2017-01-09 19:42:25 +0200

    Added solution and base tests or problem 8

---
+++ hw3-solutions/Task-8.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cassert>
+#include <cmath>
+using namespace std;
+
+const unsigned MaxSize = 100000;
+void readArray(int* data, unsigned size)
+{
+    for (unsigned i = 0; i < size; ++i)
+        cin >> data[i];
+}
+
+void printArray(const int* data, unsigned size)
+{
+    assert(size > 0);
+    for (unsigned i = 0; i < size-1; ++i)
+        cout << data[i] << " ";
+    cout << data[size-1] << endl;
+}
+
+void insertionSort(int* data, unsigned size)
+{
+    for (unsigned i = 1; i < size; ++i)
+    {
+        int h = data[i];
+        int pos;
+        for (pos = i-1; pos >= 0; --pos)
+        {
+            if ((data[pos] % 3 + 3) % 3 > (h % 3 + 3) % 3)
+                data[pos+1] = data[pos];
+            else
+                break;
+        }
+        data[pos+1] = h;
+    }
+}
+
+void sortArray(int* data, unsigned size)
+{
+    int* tmpData = new(nothrow) int[size];
+    if (!tmpData)
+    {
+        insertionSort(data, size);
+        return;
+    }
+    unsigned pos = 0;
+    for (unsigned mod = 0; mod < 3; ++mod)
+    {
+        for (unsigned i = 0; i < size; ++i)
+            if ((data[i] % 3 + 3) % 3 == mod)
+                tmpData[pos++] = data[i];
+    }
+    cout << pos << endl;
+    for (unsigned i = 0; i < size; ++i)
+        data[i] = tmpData[i];
+
+    delete[] tmpData;
+}
+
+int main()
+{
+    unsigned n;
+    cin >> n;
+    if (n == 0 || n > 100000)
+    {
+        cerr << "Bad input size" << endl;
+        return 1;
+    }
+
+    int* data = new (nothrow) int[n];
+    if (!data)
+    {
+        cerr << "No memory" << endl;
+        return 1;
+    }
+    readArray(data, n);
+
+    sortArray(data, n);
+    printArray(data, n);
+
+    delete[] data;
+    return 0;
+}
