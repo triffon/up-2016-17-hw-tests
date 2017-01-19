b92d0f8b3fbd4e995542019ec0a968d48d3e29cb
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-01-19 14:51:20 +0200

    Копиране на скриптовете за тестване при базовите тестове за третото домашно.

---
+++ hw3/test.bat
@@ -0,0 +1,46 @@
+@echo off
+
+echo ~~~~~~~~~~~~ Tests started ~~~~~~~~~~~~~
+
+for /l %%i in (1,1,10) do (
+	if not exist fn*_prob%%i_*.exe (
+		echo ========= fnXXXXX_prob%%i_CC.exe not found! =========
+	) else (
+		echo ======= Testing program %%i started ======
+		echo.
+		for %%t in (%%i?.test) do (
+			echo -------- Running test %%t ---------
+			echo.
+			echo ^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^> Input ^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>
+			type %%t
+			echo.
+			echo ^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>^>
+			echo.
+			for %%F in ("%CD%\fn*_prob%%i_*.exe") do %%~nxF < %%t >%%t.out
+			echo ^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^< Output ^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<
+			type %%t.out
+			echo.
+			echo ^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<^<
+			echo.
+			fc %%t.out %%t.ans >nul
+			if errorlevel 1 (
+				echo !!!!!!!!!!!!!!!! ERROR !!!!!!!!!!!!!!!!!
+				echo ########### Expected output ############
+				type %%t.ans
+				echo.
+				echo ########################################
+			) else (
+				echo ++++++++++++++++++ OK ++++++++++++++++++
+			)
+			echo.
+			echo -------- Test %%t finished ---------
+			pause
+		)
+		echo ====== Testing program %%i finished ======
+		pause
+	)
+	echo.
+)
+
+echo ~~~~~~~~~~~~ Tests finished ~~~~~~~~~~~~
+pause
